f1d8570b432902b816c958bd4a559ee5db473242
Jean Leles <jeanleles.sa@gmail.com>
2022-07-13 17:08:05 -0300

    Delete dist directory

--- dist/Configs/Constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-exports.config = {
-    API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://urlshtr07:CurtarCamin2029db@cluster0.sgwpr.mongodb.net/?retryWrites=true&w=majority',
-};
-//# sourceMappingURL=Constants.js.map
